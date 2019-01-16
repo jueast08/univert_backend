11561dfc3cb430251d7d02b5bf218c872306af9e
Aurélien <motta@ecole.eniscaen.fr>
2019-01-16 14:10:13 +0100

    Ajout des classes/methodes java

---
+++ src/com/univert/garden/Slot.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.univert.garden;
+
+/**
+ *
+ * @author Aurélien
+ */
+public class Slot {
+    int s_id;
+    int s_row_position;
+    int s_col_position;
+    Plant s_plant;
+    
+    public Slot(int id,int row,int col,Plant plant){
+        s_id=id;
+        s_row_position=row;
+        s_col_position=col;
+        s_plant=plant;
+    }
+}
